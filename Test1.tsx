523886daaa2d215b28e6de143fe7667dac3dde66
Jeremy Robson <Jeremy.Robson@space01.co.uk>
2025-05-13 09:05:31 +0100

    no message

--- Test1.tsx
+++ Test1.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const data = [
     { name: 'Toffees', price: 1.25 },
     { name: 'Sherbert Lemons', price: 2.30 },
-    { name: 'Pear drops', price: 2.95 },
+    { name: 'Pear Drops', price: 2.95 },
     { name: 'Butterscotch', price: 2.45 },
     { name: 'Cola Cubes', price: 9.00 },
     { name: 'Peanut Brittle', price: 8.75 },
